e565402cc136744f224b124c16672b19edd68176
José A. Múrcia <joamuran@gmail.com>
2020-07-03 08:52:54 +0200

    Fixed presint

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm *.buildinfo 2>/dev/null
+rm *.changes 2>/dev/null
+rm *.deb 2>/dev/null
+rm *.dsc 2>/dev/null
+rm *.tar.xz 2>/dev/null
+rm *.build 2>/dev/null
+rm *.upload 2>/dev/null
+
